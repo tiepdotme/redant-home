95c6220f7cc41d15908261abf607c49f7051dd1b
Michelle Yeung <michelle@redant.com.au>
2017-12-01 11:24:15 +1100

    Project gallery rework

--- src/homepage.js
+++ src/homepage.js
@@ -1,42 +1,43 @@
 import 'slick-carousel';
-import 'ekko-lightbox';
 
 import AOS from 'aos';
 
 const init = $(function() {
+  var carousel_selector = '.carousel';
   var overlay_selector = '.nav-overlay';
   var toggle_selector = '.nav-toggle-icon';
 
+  /* Nav menu */
   $(toggle_selector + ', .nav-links').click(function() {
     $(overlay_selector).toggleClass('open');
   });
 
+  /* AOS */
   $(window).resize(function() {
     AOS.refresh();
   });
 
-  function init() {
-    $('.carousel').slick({
-      dots: true,
-      appendDots: '.carousel-dots'
-    });
+  AOS.init({
+    dataAosAnchorPlacement: 'center-bottom',
+    easing: 'ease-in-out'
+  });
 
-    AOS.init({
-      dataAosAnchorPlacement: 'center-bottom',
-      easing: 'ease-in-out'
-    });
+  /* Carousel */
+  $(carousel_selector).slick({
+    dots: true,
+    appendDots: '.carousel-dots',
+    appendArrows: '.carousel-arrows'
+  });
 
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-      event.preventDefault();
-      $(this).ekkoLightbox({
-        alwaysShowClose: true,
-        leftArrow: '<i class="fa fa-chevron-left"></i>',
-        rightArrow: '<i class="fa fa-chevron-right"></i>',
-      });
-    });
-  }
+  $('#gallery-modal').on('shown.bs.modal', function (e) {
+    var $carousel = $(this).find(carousel_selector);
+    var index = $(e.relatedTarget).data('index');
+    $carousel.resize();
 
-  init();
+    if (index) {
+      $carousel.slick('slickGoTo', index - 1);
+    }
+  });
 });
 
 export default init;
