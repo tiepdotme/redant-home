b2dc9b193bd90efd019b6a7bedc30d9ce244d1f3
Paulo Pao <paulo@redant.com.au>
2020-04-29 07:03:13 +1000

    Remove legacy code

--- _legacy/scripts/index.js
+++
@@ -1,8 +0,0 @@
-//import 'node_modules/slick-carousel/slick/slick.scss';
-//import 'node_modules/slick-carousel/slick/slick-theme.scss';
-//import 'node_modules/aos/dist/aos.css';
-
-import './homepage';
-import './grid';
-
-import '../styles/index.scss';
